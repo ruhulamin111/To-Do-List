d0da5c10a00153542c061affcd8605567c97257c
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-11 07:18:03 +0600

    problem solve

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 let count = 0;
 const addItem = () => {
-    const inputValue = document.getElementById('input-field').value;
     count++;
-    if (inputValue == 0) {
+    const inputValue = document.getElementById('input-field').value;
+    if (inputValue == '') {
         alert('Please choose an item')
     }
     else {
         const container = document.getElementById('container');
-        const subContainer = document.getElementById('sub-container');
+        const subContainer = document.createElement('tr');
         subContainer.innerHTML = `
         <th scope="row">${count}</th>
-        <td>Demo</td>
+        <td>${inputValue}</td>
         <td>
-        <button onclick="" class="border-0 bg-danger text-white rounded ">Delete</button>
-        <button onclick="" class="border-0 bg-danger text-white rounded ">Done</button>
+            <button onclick="" class="border-0 bg-danger text-white rounded ">
+            Delete</button>
+            <button onclick="" class="border-0 bg-success text-white rounded ">
+            Done</button>
         </td>
 `;
+        container.appendChild(subContainer);
+        document.getElementById('input-field').value = '';
     }
 }
