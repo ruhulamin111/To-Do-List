adbe50b98671805ceb327761f69d143f5fa484d5
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-11 08:01:51 +0600

    done this site

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+// intial number 
 let count = 0;
+// add item 
 const addItem = () => {
     count++;
     const inputValue = document.getElementById('input-field').value;
@@ -20,17 +22,18 @@ const addItem = () => {
         `;
         container.appendChild(subContainer);
         document.getElementById('input-field').value = '';
-
+        // delete button work 
         const deleteButton = document.getElementsByClassName('btn-delete');
         for (const button of deleteButton) {
             button.addEventListener('click', function (event) {
                 event.target.parentNode.parentNode.style.display = 'none';
             })
         }
+        // done button work 
         const doneButton = document.getElementsByClassName('btn-done');
         for (const button of doneButton) {
             button.addEventListener('click', function (event) {
-                event.target.parentNode.parentNode.style.display = 'none';
+                event.target.parentNode.parentNode.style.backgroundColor = 'orange';
             })
         }
 
